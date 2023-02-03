4234622aba499689b68737e6d30b737ce5aa26f7
Audun Meltzer <audun.meltzer@gmail.com>
2023-02-03 13:46:05 +0100

    Starter idle og connect

--- src/main/java/no/ks/fiks/io/klient/RequestFactoryImpl.java
+++ src/main/java/no/ks/fiks/io/klient/RequestFactoryImpl.java
@@ -23,7 +23,6 @@ public class RequestFactoryImpl implements RequestFactory {
         this.portNumber = portNumber;
 
         this.client = new HttpClient(new SslContextFactory.Client());
-        this.client.setDestinationIdleTimeout(Duration.ofMinutes(1).toMillis());
         this.client.setIdleTimeout(Duration.ofMinutes(2).toMillis());
         this.client.setConnectTimeout(Duration.ofSeconds(30).toMillis());
         try {
