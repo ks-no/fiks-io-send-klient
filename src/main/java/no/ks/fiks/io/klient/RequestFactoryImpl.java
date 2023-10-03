a57c3ad3e458183e2ca012acaaf33acd73a7580f
Sondre Eikanger Kvalø <zapodot@gmail.com>
2023-10-03 08:09:52 +0200

    Optimize imports

--- src/main/java/no/ks/fiks/io/klient/RequestFactoryImpl.java
+++ src/main/java/no/ks/fiks/io/klient/RequestFactoryImpl.java
@@ -2,14 +2,10 @@ package no.ks.fiks.io.klient;
 
 import lombok.Builder;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.ClassicHttpRequest;
 import org.apache.hc.core5.http.HttpEntity;
-import org.apache.hc.core5.util.TimeValue;
 
 import java.net.URI;
-import java.time.Duration;
 
 public class RequestFactoryImpl implements RequestFactory {
     static final String BASE_PATH = "/fiks-io/api/v1/";
